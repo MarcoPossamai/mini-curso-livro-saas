878edff9ea31e3570fa3536a6c80e3b80cb5061f
MARCO ANTONIO GARLINI POSSAMAI <10292769946@edu.udesc.br>
2025-09-18 21:07:34 -0300

    commit inicial

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <nav>
+          <Image src="" alt="Livro SaaS"/>
+          <div>
+            <Button>Login</Button>
+          </div>
+        </nav>
+      </section>
+    </main>
+  );
+}
